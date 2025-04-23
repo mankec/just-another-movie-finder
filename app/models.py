b62eeaafb975ce39b381478f1b75f800a2f5c1b4
Milan Kecman <milan.kecman270@gmail.com>
2025-04-23 19:13:40 +0200

    feat: add client table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+def default_client_settings():
+    return {
+        "simkl": {
+            "access_token": "",
+            "account_id": ""
+        }
+    }
+
+class Client(models.Model):
+    logging_service = models.CharField(max_length=16, null=False, default="")
+    settings = models.JSONField(null=False, default=default_client_settings())
+
+    class Meta:
+        db_table = "client"
