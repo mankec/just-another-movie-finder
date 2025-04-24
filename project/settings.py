8bcb9e4937acb1000be7e4528ac3188af53634ff
Milan Kecman <milan.kecman270@gmail.com>
2025-04-24 02:53:52 +0200

    chore: create PostgreSQL database

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
+from environs import Env
+
+
+env = Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -70,13 +76,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": env.str("PG_DBNAME"),
+        "USER": env.str("PG_USER"),
+        "PASSWORD": env.str("PG_PASSWORD"),
+        "HOST": env.str("PG_HOST"),
+        "PORT": env.str("PG_PORT"),
     }
 }
 
