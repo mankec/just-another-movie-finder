2672310925674e33f0b4f97c9bb65c8fd40b4c79
Milan Kecman <milan.kecman270@gmail.com>
2025-04-29 23:06:48 +0200

    feat: add basic design

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "movies"
 urlpatterns = [
-    path("", views.index, name="index"),
+    # path("find-movies", views.index, name="index"),
+    path("sign-in", views.sign_in, name="sign_in"),
 ]
--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,9 @@ from django.urls import include, path
 
 from movies import views as movies_views
 
-app_name = "movies"
 urlpatterns = [
     path("", movies_views.index, name="index"), # Root
+    path("sign-in", movies_views.sign_in, name="sign_in"),
     path("movies/", include("movies.urls")),
     # path("admin/", admin.site.urls),
 ]
