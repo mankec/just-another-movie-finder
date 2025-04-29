a233913e2b609f2331ce644cbe2bbb5827098be1
Milan Kecman <milan.kecman270@gmail.com>
2025-04-29 23:06:48 +0200

    feat: add basic design

--- movies/views.py
+++ movies/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "movies/index.html")
+
+def sign_in(request):
+    return render(request, "movies/sign_in.html")
