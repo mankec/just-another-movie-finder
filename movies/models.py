025acd8b8ffc316847c5388128113b4cdfbe41b7
Nebojsa Jerkovic <185627100+nebojsa-99@users.noreply.github.com>
2025-05-08 14:52:55 +0200

    feat: add movie table

--- movies/models.py
+++ movies/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+class Movie(models.Model):
+    class Meta:
+        db_table = "movie"
+        
+    title = models.CharField(max_length=50, null=False, blank=True)
+    image = models.ImageField(upload_to='movie_posters/', null=False, blank=True)
+    runtime = models.PositiveIntegerField(null=False)
+    status = models.CharField(max_length=20, null=False, blank=True)
+    keep_updated = models.BooleanField(null=False, blank=True)
+    year = models.PositiveSmallIntegerField(null=False,)
+    tvdb_id = models.PositiveIntegerField(null=False)
+    imdb_id = models.CharField(max_length=16, null=False, blank=True)
+    tmdb_id = models.PositiveIntegerField(null=False)
+    budget = models.PositiveIntegerField(null=False)
+    box_office = models.PositiveIntegerField(null=False)
+    country = models.CharField(max_length=60, null=False, blank=True)
+    language = models.CharField(max_length=20, null=False, blank=True)
