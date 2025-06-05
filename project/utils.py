cb586c17bf486c0b2b60948f9a6f8ac979a0bbb5
Milan Kecman <milan.kecman270@gmail.com>
2025-06-05 13:18:02 +0200

    refactor: split build_url into two methods

--- project/utils.py
+++ project/utils.py
@@ -3,9 +3,11 @@ from urllib.parse import urlencode
 
 from requests import get, post, RequestException
 
-def build_url(url, query={}):
-    if not query:
-        return url
+def build_url(*path_segments):
+    return "/".join(str(path_segment).strip("/") for path_segment in path_segments)
+
+
+def build_url_with_query(url, query):
     return f"{url}?{urlencode(query)}"
 
 
