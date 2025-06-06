41dde18ee6f4d9cedd5bca6cc45f8141b7da8607
Milan Kecman <milan.kecman270@gmail.com>
2025-06-06 23:18:14 +0200

    fix: revert deleting vite.config.mjs

---
+++ vite.config.mjs
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: "./static/build",
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './src/js/main.js'),
+      }
+    }
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+})
