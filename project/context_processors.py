afc43306fc72492c2de6f56dd9d2c3201ccec66d
Milan Kecman <milan.kecman270@gmail.com>
2025-06-16 16:16:56 +0200

    test: user is redirected from sign_in page if already signed in

--- project/context_processors.py
+++ project/context_processors.py
@@ -1,4 +1,4 @@
-from project.auth.utils import is_signed_in
+from auth.utils import is_signed_in
 
 
 def is_signed_in_cp(request):
--- project/urls.py
+++ project/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 from movies import views as movies_views
+from auth import views as auth_views
 
 urlpatterns = [
     path("", movies_views.index, name="index"), # Root
-    path("sign-in", movies_views.sign_in, name="sign_in"),
-    path("sign-out", movies_views.sign_out, name="sign_out"),
+    path("sign-in", auth_views.sign_in, name="sign_in"),
+    path("sign-out", auth_views.sign_out, name="sign_out"),
     # TODO: Put this in core app
     path('error/', movies_views.error, name='error'),
     path("movies/", include("movies.urls")),
