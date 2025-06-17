a4d01dcffb80c8e313bc173b252b0e0103446271
Milan Kecman <milan.kecman270@gmail.com>
2025-06-17 23:04:54 +0200

    feat: improve Simkl and Trakt APIs

--- movie_loggers/services/simkl.py
+++ movie_loggers/services/simkl.py
@@ -1,101 +1,132 @@
-from http import HTTPMethod
+from http import HTTPMethod, HTTPStatus
 
 from environs import Env
+from requests import Response
+from requests.exceptions import HTTPError
 from django.contrib.sessions.models import Session
 
 from project.settings import API_REDIRECT_URL
+from core.wrappers import handle_exception
 from core.url.utils import build_url, build_url_with_query
 from core.request.utils import send_request
-from movie_loggers.services.base import AbstractMovieLogger
+from movies.models import Movie
+from movie_loggers.services.base import MovieLogger, AbstractMovieLogger
 
 env = Env()
 
 
 class Simkl(AbstractMovieLogger):
+    API_URL = "https://api.simkl.com"
+    CLIENT_ID = env.str("SIMKL_CLIENT_ID", "")
+    CLIENT_SECRET = env.str("SIMKL_CLIENT_SECRET", "")
+    MOVIE_STATUS_PLANTOWATCH = "plantowatch"
+    REQUIRED_HEADERS = {
+        "simkl-api-key": CLIENT_ID,
+    }
+
     def __init__(self, session: Session):
         self.session = session
-        self.name = session["movie_logger"]
-        self.api_url = "https://api.simkl.com"
-        self.client_id = env.str("SIMKL_CLIENT_ID", "")
-        self.client_secret = env.str("SIMKL_CLIENT_SECRET", "")
-        self.redirect_uri = API_REDIRECT_URL
+        self.name = MovieLogger.SIMKL.value
 
-    def authorize_application_url(self):
+    @handle_exception("Something went wrong while trying to sign you in to Simkl.")
+    def authorize_application_url(self) -> str:
         url = "https://simkl.com/oauth/authorize"
         query = {
             "response_type": "code",
-            "client_id": self.client_id,
-            "redirect_uri": self.redirect_uri,
+            "client_id": self.CLIENT_ID,
+            "redirect_uri": API_REDIRECT_URL,
             "movie_logger": self.name
         }
         return build_url_with_query(url, query)
 
-    def obtain_token(self, *, code):
-        url = build_url(self.api_url, "oauth/token")
+    @handle_exception("Something went wrong while trying to sign you in to Simkl.")
+    def obtain_token(self, *, code) -> bool:
+        url = build_url(self.API_URL, "oauth/token")
         payload = {
             "code": code,
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-            "redirect_uri": self.redirect_uri,
+            "client_id": self.CLIENT_ID,
+            "client_secret": self.CLIENT_SECRET,
+            "redirect_uri": API_REDIRECT_URL,
             "grant_type": "authorization_code",
-        }
-        try:
-            response = send_request(
-                method=HTTPMethod.POST.name,
-                url=url,
-                payload=payload,
-            )
-            response_body = response.json()
-        except Exception:
-            raise Exception("Something went wrong while trying to sign you in.")
-        self.session["token"] = response_body["access_token"]
-        return "Successfully signed to Simkl!"
-
-    def add_to_watchlist(self, movie):
-        movie_data = self._fetch_movie(movie)
-
-        if not movie_data: return
-
-        movie_data["to"] = "plantowatch"
-        movie_data["ids"]["tvdb"] = movie["id"]
-
-        url = build_url(self.api_url, "sync/add-to-list")
-        headers = {
-            'Authorization': f"Bearer {self.session["token"]}",
-            'simkl-api-key': self.client_id
-        }
-        payload = {
-            "movies": [movie_data],
         }
         response = send_request(
             method=HTTPMethod.POST.name,
             url=url,
-            headers=headers,
             payload=payload,
         )
+        response_body = response.json()
+        self.session["token"] = response_body["access_token"]
+        return True
 
-    def _fetch_movie(self, movie: dict) -> dict:
-        url = build_url(self.api_url, "search/id")
+    @handle_exception("Something went wrong.")
+    def add_to_watchlist(self, movie: Movie) -> bool:
+        try:
+            url = build_url(self.API_URL, "sync/add-to-list")
+            payload = {
+                "movies": [self._movie_data(movie)],
+            }
+            response = send_request(
+                method=HTTPMethod.POST.name,
+                url=url,
+                headers=self._oauth_required_headers(),
+                payload=payload,
+            )
+            response_body = response.json()
+            if response_body["not_found"]["movies"]:
+                response = Response()
+                response.status_code = HTTPStatus.NOT_FOUND
+                raise HTTPError(response=response)
+            return True
+        except HTTPError as error:
+            response = error.response
+            if response.status_code == HTTPStatus.NOT_FOUND:
+                message = f"Simkl couldn't find '{movie.title}'."
+            else:
+                message = f"Someting went wrong while trying to add '{movie.title}' to your Trakt's watchlist."
+            raise HTTPError(message)
+
+    @handle_exception
+    def fetch_movie_ids_in_watchlist(self) -> list:
+        movie_ids = []
+        url = build_url(self.API_URL, "sync/all-items")
         payload = {
-            # "tvdb": tvdb_id(movie["id"], movie["title"]),
-            "title": movie["title"],
-            "year": movie["year"],
-            "client_id": self.client_id,
+            "type": "movies",
+            "status": self.MOVIE_STATUS_PLANTOWATCH,
+            "extended": "ids_only",
         }
-        response = send_request(
+        response =  send_request(
             method=HTTPMethod.GET.name,
             url=url,
             payload=payload,
+            headers=self._oauth_required_headers(),
         )
-        data = response.json()
+        response_body = response.json()
+        if not response_body:
+            return []
+        movies = response_body["movies"]
+        movie_ids = [
+            {
+                "imdb_id": d["movie"]["ids"]["imdb"],
+                "tmdb_id": d["movie"]["ids"]["tmdb"]
+            }
+            for d in movies
+        ]
+        return movie_ids
 
-        if not data: return
+    def _movie_data(self, movie: Movie) -> dict:
+        return {
+            "to": self.MOVIE_STATUS_PLANTOWATCH,
+            "title": movie.title,
+            "year": movie.year,
+            "ids": {
+                "tvdb": movie.tvdb_id,
+                "imdb": movie.imdb_id,
+                "tmdb": movie.tmdb_id,
+            }
+        }
 
-        if len(data) > 1:
-            # TODO:
-            # This should never happen but if it does we must catch it and then fix it.
-            # Set up email service that will notify us in case this happens.
-            raise RuntimeError(
-                "Only one movie must be retrieved: Simkl API, search/id"
-            )
-        return data.pop()
+    def _oauth_required_headers(self):
+        return {
+            **self.REQUIRED_HEADERS,
+            'Authorization': f"Bearer {self.session["token"]}",
+        }
