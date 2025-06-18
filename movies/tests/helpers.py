8cb48b905d76767df7faed372c75e0072b19dfc8
Milan Kecman <milan.kecman270@gmail.com>
2025-06-18 17:22:24 +0200

    feat: create MovieFinder

---
+++ movies/tests/helpers.py
@@ -0,0 +1,12 @@
+from copy import deepcopy
+
+from movies.models import Movie
+
+
+def create_dummy_movie(original):
+    num = (Movie.objects.count() + 1)
+    new_movie = deepcopy(original)
+    new_movie.tvdb_id = num
+    new_movie.slug = f"dummy-movie-{num}"
+    new_movie.save()
+    return new_movie
