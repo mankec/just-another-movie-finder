d77a7ee09211636825ef343f509aa0f3a5eb2dfb
Milan Kecman <milan.kecman270@gmail.com>
2025-06-19 15:16:55 +0200

    refactor: use core/index.html as root page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+def index(request):
+    return render(request, "core/index.html")
+
+
 def error(request):
     ctx = {
         "headerless": True
--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from movies import views as movies_views
 from oauth import views as auth_views
 from core import views as core_views
 
 urlpatterns = [
-    path("", movies_views.index, name="index"), # Root
+    path("", core_views.index, name="index"), # Root
     path("sign-in", auth_views.sign_in, name="sign_in"),
     path("sign-out", auth_views.sign_out, name="sign_out"),
     path('error/', core_views.error, name='error'),
+    path("core/", include("core.urls")),
     path("oauth/", include("oauth.urls")),
     path("movies/", include("movies.urls")),
     path("admin/", admin.site.urls),
