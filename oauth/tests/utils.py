1188547b970534257ec49f935baf4ba3b08d5b8b
Milan Kecman <milan.kecman270@gmail.com>
2025-06-25 15:01:01 +0200

    test: fail test if NoReverseMatch is raised in selenium_sign_in_user

--- oauth/tests/utils.py
+++ oauth/tests/utils.py
@@ -1,5 +1,6 @@
 from django.contrib.sessions.models import Session
 from django.urls import reverse
+from django.urls.exceptions import NoReverseMatch
 from django.test import TestCase
 
 from movie_loggers.tests.services.constants import DEFAULT_TEST_MOVIE_LOGGER
@@ -13,7 +14,10 @@ def sign_in_user(session: Session):
 
 # TODO: Perhaps change this to mixin? If you do, add also selenium_url that will prepend live_server_url to target url.
 def selenium_sign_in_user(test_case: TestCase, movie_logger):
-    url = reverse("oauth:selenium_sign_in", kwargs={
-        "movie_logger": movie_logger,
-    })
-    test_case.selenium.get(f"{test_case.live_server_url}/{url}")
+    try:
+        url = reverse("oauth:selenium_sign_in", kwargs={
+            "movie_logger": movie_logger,
+        })
+        test_case.selenium.get(f"{test_case.live_server_url}/{url}")
+    except NoReverseMatch:
+        test_case.fail("This URL is only available in test environment. Run `DJANGO_ENV=test python manage.py test` to be able to access it.")
