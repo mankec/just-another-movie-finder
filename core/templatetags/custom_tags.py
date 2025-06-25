9f49d5da1a5e0202d850ac831b488cb64240ae14
Milan Kecman <milan.kecman270@gmail.com>
2025-06-25 15:39:59 +0200

    feat: improve flash message

--- core/templatetags/custom_tags.py
+++ core/templatetags/custom_tags.py
@@ -40,11 +40,11 @@ def flash_message_html(tags):
           x-init="setTimeout(() => show = false, 10000);"
           x-show="show"
           x-transition
-          class="px-4 py-3 {tags}"
+          class="relative px-4 py-3 {tags}"
           role="alert"
         >
-          <span id="flash-message-text" class="block sm:inline">__MESSAGE__</span>
-          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <span id="flash-message-text">__MESSAGE__</span>
+          <span class="absolute top-0 right-0">
             <svg
               @click="show = false"
               class="{DEFAULT_MESSAGE_TAGS} {_xmark_icon_color(tags)}" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
