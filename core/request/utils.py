ebbcca7681bbf863b928ca6cb001d93b01a1fde2
Milan Kecman <milan.kecman270@gmail.com>
2025-06-26 11:45:41 +0200

    test: raise if request isn't stubbed in test environment

--- core/request/utils.py
+++ core/request/utils.py
@@ -3,10 +3,18 @@ from http import HTTPMethod
 from requests import get, post
 
 from core.wrappers import handle_exception
+from core.environments.utils import is_test
+from project.settings import SKIP_EXTERNAL_TESTS
 
 
 @handle_exception
 def send_request(*, method, url, headers={}, payload={}):
+    if is_test() and SKIP_EXTERNAL_TESTS.value:
+        raise RuntimeError("""
+        Attempted to send a real request in test environment.
+        Stub it or disable SKIP_EXTERNAL_TESTS.
+        """
+        )
     response = None
     match method:
         case HTTPMethod.GET.value:
