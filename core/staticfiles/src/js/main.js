6006f93adc92a0d8fa175de66ed57b9ad076f5b2
Milan Kecman <milan.kecman270@gmail.com>
2025-09-02 22:06:23 +0200

    chore: run collectstatic for the first time

---
+++ core/staticfiles/src/js/main.js
@@ -0,0 +1,10 @@
+// Load Tailwind CSS and custom CSS
+import '../styles/main.css';
+
+import Alpine from 'alpinejs'
+import "./flash_message.js"
+import "./search_select_multiple.js"
+
+window.Alpine = Alpine
+
+Alpine.start()
